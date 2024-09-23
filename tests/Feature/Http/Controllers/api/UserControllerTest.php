950651e3bcde7882345a0aea009c4ed149248d92
AdsonBruno <adsonbruno2@gmail.com>
2024-09-23 09:19:57 -0300

    test: should return 422 if username is not passed

---
+++ tests/Feature/Http/Controllers/api/UserControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\api;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testShouldReturn422IfTheNameIsNotPassed() 
+    {
+        $response = $this->postJson('api/users', [
+            'email' => 'test@exemple.com',
+            'password' => 'password123',
+            'creci_number' => '1234567',
+            'profile_picture' => 'http://url.com.br',
+            'city' => 'Arapiraca',
+            'state' => 'Alagoas',
+            'date_of_birth' => '07011997',
+            'is_verified' => 'true'
+        ]);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors('name');
+    }
+}
